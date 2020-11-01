10dd7f9c3387a8565a1c2dc0c5ec0207690eea71
dahaliahowell <dahaliahowell.dh@gmail.com>
2020-10-31 20:20:49 -0500

    Added more styles

--- scripts.js
+++ scripts.js
@@ -3,17 +3,23 @@ window.onload = function(){
     headerDiv = document.querySelectorAll('header > div');
     abtDiv = document.querySelectorAll('.learn-about > .container');
     cards = document.querySelectorAll('.learn-about > .card');
+    abth2 = document.querySelectorAll('.learn-about > h2');
 
     headerDiv.forEach(element => {
         element.classList.add('headerdiv');
     });
 
+    abth2.forEach(element => {
+        element.classList.add('header');
+    });
+
+
     abtDiv.forEach(element => {
         element.classList.add('about-grid')
     });
 
-    cards.forEach(element => {
-        element.classList.add('about-grid card')
+    cards.forEach(function(element, index, list) {
+        element.classList.add('card' + index)
     });
 
     navHeader = document.querySelectorAll('.headerdiv > h1');
